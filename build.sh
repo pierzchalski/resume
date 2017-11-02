91bcfb54883147ab818b4456e965f454050a2db6
pierzchalski <edward@pierzchalski.org>
2017-11-02 23:40:49 +1100

    add cover letter

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ latexmk -pdf \
     -pdflatex="lualatex %O --shell-escape %S" \
     -latex="lualatex %O --shell-escape %S" \
     resume.tex
+
+latexmk -pdf \
+    -pdflatex="lualatex %O --shell-escape %S" \
+    -latex="lualatex %O --shell-escape %S" \
+    cover-letter/cover_letter.tex
