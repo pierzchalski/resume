e879ea6d57d0ea16a488d32ca8eab94005effa01
pierzchalski <edward@pierzchalski.org>
2017-11-06 00:46:38 +1100

    separate resume from root dir

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
-latexmk -pdf \
-    -pdflatex="lualatex %O --shell-escape %S" \
-    -latex="lualatex %O --shell-escape %S" \
-    resume.tex
+(
+    cd cv
+    latexmk -pdf \
+        -pdflatex="lualatex %O --shell-escape %S" \
+        -latex="lualatex %O --shell-escape %S" \
+        resume.tex
+)
 
-cd cover_letter
-latexmk -pdf \
-    -pdflatex="lualatex %O --shell-escape %S" \
-    -latex="lualatex %O --shell-escape %S" \
-    cover_letter.tex
+(
+    cd cover_letter
+    latexmk -pdf \
+        -pdflatex="lualatex %O --shell-escape %S" \
+        -latex="lualatex %O --shell-escape %S" \
+        cover_letter.tex
+)
